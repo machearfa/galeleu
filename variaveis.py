939973880ce7d160e52fedfa42136a835ec5c8f0
machearfa <vitorpalomo.galileu@gmail.com>
2025-05-29 17:34:07 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,5 @@
+#vou usar input() para armazenar informação em uma variavel
+nome = input('qual nome você quer armazenar?: ')
+
+#vou usar o print para armazenar a informação da variavel nome casa do vininini
+print(f'o nome é: {nome}')
