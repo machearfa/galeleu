bc04fa2d2d324cacdc1ac8c87b9d802f9e7e7a5c
machearfa <vitorpalomo.galileu@gmail.com>
2025-06-05 17:31:20 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,10 @@
+def formula_maluca(numero):
+    return print(numero + (1 * 10 - 20 / 2 + 500))
+
+#vou chamar a funçao
+formula_maluca(4)
+
+def texto_noob(nome):
+    print(f'o {nome} é noob no fortnite')
+
+texto_noob('vitor')
