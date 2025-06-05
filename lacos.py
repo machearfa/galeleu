4d8aed6d4842fab741e504e5dc9639e58a1e8bdd
machearfa <vitorpalomo.galileu@gmail.com>
2025-06-05 17:09:40 -0300

    Add files via upload

---
+++ lacos.py
@@ -0,0 +1,13 @@
+#criar uma variavel chamada soma
+#soma = 0 
+#for o in range(10):
+  #  soma = soma + 1
+  #  print(o)
+
+
+#while True:
+    #print()
+soma = 0 
+while soma < 1000: # enquanto soma for menor que 1000
+    soma = soma + 10 #soma o valor antigo
+    print(soma) #logo em seguida printa o resultado em tela
