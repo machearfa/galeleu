3fd5521535c0cd8f6d1fab058ed8c8e900c7161b
machearfa <vitorpalomo.galileu@gmail.com>
2025-06-12 17:36:12 -0300

    Add files via upload

--- strings2.py
+++ strings2.py
@@ -1,1 +1,17 @@
 texto = "esse é o meu texto AfJjfkKl OnIcjOe e Eou OUe ViNee NãoOOOo ResPiRRoU Ee FeiSZ 32 AnOs AaAASDA@@@@"
+print(texto.capitalize())
+
+print(texto.title())
+
+nome = 'vitor'
+print(nome.center(40, '='))
+
+asd = len(texto)
+print(asd)
+
+print(texto.lower())
+
+print(texto.upper())
+
+frase = 'asdoaisd'
+print(frase.replace('asdoaisd', 'sajdkasjdka'))
