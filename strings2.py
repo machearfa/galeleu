3216c4dbcfc97e2a06a1a1e6ba230fe2be465f66
machearfa <vitorpalomo.galileu@gmail.com>
2025-06-17 17:13:24 -0300

    Add files via upload

--- strings2.py
+++ strings2.py
@@ -15,3 +15,40 @@ print(texto.upper())
 
 frase = 'asdoaisd'
 print(frase.replace('asdoaisd', 'sajdkasjdka'))
+
+arquivo = 'skins_vitor.csv'
+
+#metodo.startswith(palavra), retorna se a palavra existe no inicio da variavel
+print(arquivo.startswith('skins'))
+#metodo.endswith(palavra), retorna se a palavra existe no inicio da variavel
+print(arquivo.endswith('.py'))
+
+nick = 'machearfa235447'
+#metodo .isalnum() -> verifica se o texto possui letras e numeros APENAS
+print(nick.isalnum())
+
+#metodo .isalpha() -> verifica se o texto possui APENAS letras
+print(nick.isalpha())
+
+#metodo .isdigit() -> verifica se o texto possui APENAS numeros
+print(nick.isdigit())
+
+#metodo .strip() -> remove os espaços de um texto
+senha = 'kikinho2014'
+senhaTentativa = input('digite sua senha: ').strip()
+
+#verifique se a senha é igual a senha tentativa
+if senha == senhaTentativa:
+    print("senha correta")
+else:
+    print('senha incorreta')
+
+
+#crie uma variavel chamada frase_torta
+#armazene nessa variavel uma frase que contenha letras minusculas, maiusculas, espaços nas extremidades
+#crie uma outra variavel chamada frase_torta_formatada
+#utilize o encadeamento de métodos para deixar a frase mais limpa
+
+frase_torta = "frAse boNita"
+frase_torta_formatada = frase_torta.lower().strip().replace('bonita', 'feia')
+print(frase_torta_formatada)
