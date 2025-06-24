4e936e997e0489d6eaee9e85a49b39b235d5ad01
machearfa <vitorpalomo.galileu@gmail.com>
2025-06-24 17:35:25 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,18 @@
+lista_media_notas = [1.5, 
+                     0.1, 
+                     1.6, 
+                     2.0]
+
+#comando sum(), soma todos os elementos de uma lista
+#comando len(), retorna todos os elementos de uma lista
+
+media = sum(lista_media_notas) / len(lista_media_notas)
+print(media)
+
+lista_nomes = ['MarchaAlfa', "abujabu", 'vine', 'nichi']
+
+#imprimi o abujabu
+import time
+print(lista_nomes[1])
+time.sleep(10)
+print(lista_nomes[-1])
