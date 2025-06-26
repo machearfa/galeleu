f263bfd30b9687ac979e39c14192b65bc01e83dd
machearfa <vitorpalomo.galileu@gmail.com>
2025-06-26 17:34:37 -0300

    Add files via upload

---
+++ galiGPT.py
@@ -0,0 +1,12 @@
+inputp = input('bem vindo ao galiGPT digite oque quer : ')
+
+if inputp == 'oi':
+    print('olá')
+elif inputp == 'olá':
+    print('olá')
+elif inputp == 'bom dia':
+    print('bom dia')
+elif inputp == 'meda um real':
+    print('me desculpe, sou uma inteligencia artificial, não tenho dinheiro, nem mãos para te dar um dinheiro mas VOU TE DAR 1 CONTO NO PIXIS')
+elif inputp == 'não':
+    print('desculpe sou ainda em versão beta, me desculpe se ocorreu algo, não consigo responder isso')
